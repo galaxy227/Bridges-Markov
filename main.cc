388b7c36de588d06dc272a94e5fb781c1e1de033
a <a>
2025-04-01 01:56:12 -0700

    Logged Homework Date: Tue  1 Apr 01:56:12 PDT 2025

--- main.cc
+++ main.cc
@@ -74,6 +74,7 @@ int main() {
 		for (int i = 0; i < static_cast<int>(line.length()); i++) {
 			if (!isalpha(line[i]) && line[i] != ',' && line[i] != '.' && line[i] != '?' && line[i] != '!' && line[i] != ' ') continue;
 			if (line[i] != ' ') { word += line[i]; continue; }
+			if (word.empty()) continue;
 			for (char &c : word) c = toupper(c);
 			remove_brackets(word);
 			// Handle punctuation
@@ -84,7 +85,6 @@ int main() {
 				is_comma = true;
 			}
 			if (last_char == '.' || last_char == '?' || last_char == '!') word = word.substr(0, word.size() - 1);
-			if (word.empty()) continue;
 			// If word does NOT exist in data
 			if (word_map.find(word) == word_map.end()) {
 				word_map[word] = vertex_vector.size();
