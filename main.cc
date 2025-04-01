49bf2e71e3bed422eb0c1bc6d8d7d122ed95e8db
a <a>
2025-04-01 01:50:44 -0700

    Logged Homework Date: Tue  1 Apr 01:50:44 PDT 2025

--- main.cc
+++ main.cc
@@ -72,7 +72,7 @@ int main() {
 		uint32_t prev_index = 0;
 		// For each word in the line
 		for (int i = 0; i < static_cast<int>(line.length()); i++) {
-			if (line[i] == '=') continue;
+			if (!isalpha(line[i]) && line[i] != '.' && line[i] != '?' && line[i] != '!' && line[i] != ' ') continue;
 			if (line[i] != ' ') { word += line[i]; continue; }
 			if (word.empty()) continue;
 			for (char &c : word) c = toupper(c);
